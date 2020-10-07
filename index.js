87905b8e73fdd7387f210c089836e5a2db0a3d6c
Frumpy <chris.johnson9638@gmail.com>
2020-10-07 00:34:50 -0400

    added help command

--- index.js
+++ index.js
@@ -359,6 +359,16 @@ bot.on('message', async message => {//public commands
 
     }
 
+    if(command == "help" || command == "commands") {
+        let embed = new Discord.MessageEmbed()
+        .setTitle(`List of commands`)
+        .setColor(7980240)
+        .setTimestamp(Date.now())
+        .addField("--players/--p", "`--players <server>`\nThis command will return a list of currently connected users to the specified server.")
+        .addField("--map/--m", "`--map <Server>`\nThis command return with what map a server is on, along with any other relevant information about the map.")
+
+        message.channel.send({embed: embed})
+    }
 })
 
 
